f6436f955062bd67c6b0495fe1c7cd67e0234796
SREENIVASAN-PRASANTH <prasanthshot@gmail.com>
2024-01-26 14:46:21 +0530

    created the first page

---
+++ script.js
@@ -0,0 +1,7 @@
+function openLinkedIn(){
+    window.open("https://www.linkedin.com/in/s-prasanth2003/","_blank").focus();
+}
+
+function openGithub(){
+    window.open("https://github.com/SREENIVASAN-PRASANTH/","_blank").focus();
+}
